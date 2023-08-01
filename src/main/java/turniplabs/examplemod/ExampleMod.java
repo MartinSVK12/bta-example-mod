05b4f2ca0b0f95fb4d8999c79c562453d44d46a1
MartinSVK12 <37455793+MartinSVK12@users.noreply.github.com>
2023-08-01 20:07:03 +0200

    bruh

--- src/main/java/turniplabs/examplemod/ExampleMod.java
+++ src/main/java/turniplabs/examplemod/ExampleMod.java
@@ -1,13 +1,8 @@
 package turniplabs.examplemod;
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.client.sound.block.BlockSounds;
-import net.minecraft.core.Global;
-import net.minecraft.core.block.Block;
-import net.minecraft.core.block.material.Material;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-//import turniplabs.halplibe.helper.BlockHelper;
 
 
 public class ExampleMod implements ModInitializer {
